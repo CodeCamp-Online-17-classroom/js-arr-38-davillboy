546612810934491264c29d21da8d3023e2c0b829
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 00:22:03 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+const array = ['apple', 'banana', 'orange', 'pineapple', 'watermeon'];
+const result = array.filter(item => item.length > 6);
+console.log(result); // ['pineapple', 'watermeon']
+
+const array = [1, -3, 2, 8, -4, 5];
+const result = array.filter(item => item > 0);
+console.log(result); // [1, 2, 8, 5]
+
+const array = [1, 3, 4, 5, 6, 7, 8];
+const result = array.filter(item => item % 3 === 0);
+console.log(result); // [3, 6]
